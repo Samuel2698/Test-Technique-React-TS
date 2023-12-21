83b535a774a9d351851efbd8a2b4fb9b75ccab07
Samuel Velasco <samuelvelasco2698@gmail.com>
2023-12-21 13:22:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>TS Test</h1>
+      </header>
+      <main>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
+          voluptatibus vitae rerum quidem tenetur veritatis qui voluptatum a
+          laborum quia animi itaque facere ipsum saepe, dolore sapiente? Quas,
+          et eius.
+        </p>
+      </main>
+    </>
+  )
+}
+
+export default App
